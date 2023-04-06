7ed3ce3f0c285a89e41f62a95d440ca6a6f4bedf
MrBaskan <mrbaskan33@gmail.com>
2023-04-06 18:36:03 +0300

    Update ai.js

--- ai.js
+++ ai.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: "API KEY",
 });
 
 const openai = new OpenAIApi(configuration);
